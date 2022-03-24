581fb5ccb82f1c1d0867b28c1e77206986f14d88
Neutron <32855909+NeutronX-dev@users.noreply.github.com>
2022-03-24 09:55:35 -0400

    v0.2 Update

--- js/init.js
+++ js/init.js
@@ -2,29 +2,33 @@ window.KaNa = {
     cookie_reader: null,
     settings: {},
     main: null,
-    DOM: {
-        display_symbol: null,
-        answer_input: null
-    }
+    DOM: {}
 }
 
 window.addEventListener("load", (ev) => {
     // Main
     window.KaNa.DOM.display_symbol = document.getElementById("display_symbol");
+    window.KaNa.DOM.answer_holder = document.getElementById("answer_holder");
+    window.KaNa.DOM.correct_answer = document.getElementById("correct_answer");
     window.KaNa.DOM.answer_input = document.getElementById("answer_input");
     window.KaNa.DOM.score = document.getElementById("score");
     // Settings
     window.KaNa.DOM.hiragana_enabled = document.getElementById("hiragana_toggle");
     window.KaNa.DOM.katakana_enabled = document.getElementById("katakana_toggle");
+
+    
     document.getElementById("settings").setAttribute("style", "display:none;")
+    window.KaNa.DOM.answer_holder.setAttribute("style", "display:none;");
+
+
 
     window.KaNa.DOM.hiragana_enabled.onchange = (ev) => {
         window.KaNa.settings.setting.hiragana = window.KaNa.DOM.hiragana_enabled.checked;
-        window.KaNa.settings.save()
+        window.KaNa.settings.save();
     };
 
     window.KaNa.DOM.katakana_enabled.onchange = (ev) => {
         window.KaNa.settings.setting.katakana = window.KaNa.DOM.katakana_enabled.checked;
-        window.KaNa.settings.save()
+        window.KaNa.settings.save();
     };
 })
--- js/main.js
+++ js/main.js
@@ -18,15 +18,16 @@ function animateValue(obj, start, end, duration) {
 window.KaNa.main = new (class {
     constructor() {
         this.symbols = [
-            ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ゃ', 'ゅ', 'ょ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'ゐ', 'ゑ', 'を', 'が', 'ぎ', 'ぐ', 'げ', 'ご', 'ざ', 'じ', 'ず', 'ぜ', 'ぞ', 'だ', 'ぢ', 'づ', 'で', 'ど', 'ば', 'び', 'ぶ', 'べ', 'ぼ', 'ぱ', 'ぴ', 'ぷ', 'ぺ', 'ぽ', 'ゔ', 'ん'],
-            ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ゃ', 'ゅ', 'ょ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヰ', 'ヱ', 'ヲ', 'ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'ダ', 'ジ', 'ズ', 'デ', 'ド', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'パ', 'ピ', 'プ', 'ペ', 'ポ', 'v', 'ン'],
-            ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ゃ', 'ゅ', 'ょ', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wi', 'we', 'wo', 'ga', 'gi', 'gu', 'ge', 'go', 'za', 'ji', 'zu', 'ze', 'zo', 'da', 'ji', 'zu', 'de', 'do', 'ba', 'bi', 'bu', 'be', 'bo', 'pa', 'pi', 'pu', 'pe', 'po', 'v', 'n']
+            ['あ', 'い', 'う', 'え', 'お', 'か', 'き', 'く', 'け', 'こ', 'さ', 'し', 'す', 'せ', 'そ', 'た', 'ち', 'つ', 'て', 'と', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ひ', 'ふ', 'へ', 'ほ', 'ま', 'み', 'む', 'め', 'も', 'や', 'ゆ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'わ', 'ゐ', 'ゑ', 'を', 'が', 'ぎ', 'ぐ', 'げ', 'ご', 'ざ', 'じ', 'ず', 'ぜ', 'ぞ', 'だ', 'ぢ', 'づ', 'で', 'ど', 'ば', 'び', 'ぶ', 'べ', 'ぼ', 'ぱ', 'ぴ', 'ぷ', 'ぺ', 'ぽ', 'ゔ', 'ん'],
+            ['ア', 'イ', 'ウ', 'エ', 'オ', 'カ', 'キ', 'ク', 'ケ', 'コ', 'サ', 'シ', 'ス', 'セ', 'ソ', 'タ', 'チ', 'ツ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'ヒ', 'フ', 'ヘ', 'ホ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ワ', 'ヰ', 'ヱ', 'ヲ', 'ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'ダ', 'ジ', 'ズ', 'デ', 'ド', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'パ', 'ピ', 'プ', 'ペ', 'ポ', 'v', 'ン'],
+            ['a', 'i', 'u', 'e', 'o', 'ka', 'ki', 'ku', 'ke', 'ko', 'sa', 'shi', 'su', 'se', 'so', 'ta', 'chi', 'tsu', 'te', 'to', 'na', 'ni', 'nu', 'ne', 'no', 'ha', 'hi', 'fu', 'he', 'ho', 'ma', 'mi', 'mu', 'me', 'mo', 'ya', 'yu', 'yo', 'ra', 'ri', 'ru', 're', 'ro', 'wa', 'wi', 'we', 'wo', 'ga', 'gi', 'gu', 'ge', 'go', 'za', 'ji', 'zu', 'ze', 'zo', 'da', 'ji', 'zu', 'de', 'do', 'ba', 'bi', 'bu', 'be', 'bo', 'pa', 'pi', 'pu', 'pe', 'po', 'v', 'n']
         ]
         // 0 = hiragana
         // 1 = katakana
         // 2 = romanji
 
         this.score = 0;
+        this.displaying_answer = false;
         this.alphabet = 0;
         this.index = 0;
 
@@ -43,7 +44,29 @@ window.KaNa.main = new (class {
             console.log("[KaNa]: Unable to load Audio");
         }
     }
+    DisplayCorrectAnswer(answer) {
+        window.KaNa.DOM.answer_input.disabled = true;
+        answer = (answer == undefined) ? this.symbols[2][this.index] : answer;
+        try {
+            window.KaNa.DOM.answer_holder.setAttribute("style", "");
+            window.KaNa.DOM.correct_answer.innerText = answer;
+            this.displaying_answer = true;
+        } catch (e) {
+            console.log("[KaNa]: Unable to display correct Answer", e);
+        }
+    }
+    HideCorrectAnswer() {
+        window.KaNa.DOM.answer_input.disabled = false;
+        try {
+            window.KaNa.DOM.answer_holder.setAttribute("style", "display:none;");
+            this.displaying_answer = false;
+        } catch (e) {
+            console.log("[KaNa]: Unable to hide correct Answer", e);
+        }
+        window.KaNa.DOM.answer_input.focus();
+    }
     Next() {
+        window.KaNa.DOM.answer_input.focus();
         if (window.KaNa.settings.setting.hiragana && window.KaNa.settings.setting.katakana) {
             this.alphabet = Math.floor(Math.random() * 2);
         } else if (window.KaNa.settings.setting.hiragana) {
@@ -77,13 +100,13 @@ window.KaNa.main = new (class {
         }
     }
     Incorrect() {
-        if(this.score >= 50){
+        if (this.score >= 50) {
             this.AddScore(-50);
         } else {
             this.AddScore(-this.score)
         }
+        this.DisplayCorrectAnswer()
         this.audio.incorrect.play()
-        this.Next()
     }
 })
 
@@ -94,7 +117,18 @@ document.addEventListener("keyup", (ev) => {
     window.KaNa.DOM.answer_input.value = val.toLowerCase().split(" ").join("");
     switch (ev.key.toLowerCase()) {
         case "enter":
-            window.KaNa.main.Verify(window.KaNa.DOM.answer_input.value);
+            if (!window.KaNa.main.displaying_answer) {
+                window.KaNa.main.Verify(window.KaNa.DOM.answer_input.value);
+            } else {
+                window.KaNa.main.HideCorrectAnswer();
+                window.KaNa.main.Next();
+            }
+            break;
+        default:
+            if (window.KaNa.DOM.answer_input.value == window.KaNa.main.symbols[2][window.KaNa.main.index]) {
+                window.KaNa.main.Verify(window.KaNa.DOM.answer_input.value)
+            }
             break;
     }
 });
+
